bd2a2a995d06771be2cb30e767ab3abcd2d2c53a
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-22 13:57:07 +0530

    learning react from project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {BrowserRouter as Router, Route} from "react-router-dom" 
+function App() {
+  
+  return (
+    <>
+    <Router>
+      
+    </Router>
+    </>
+  )
+}
+
+export default App
