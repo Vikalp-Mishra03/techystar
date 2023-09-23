58c9521d8258bb9ad944ddf2e0e21b7ed4a84ddc
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-23 12:06:20 +0530

    starting building contact page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./component/Header"
 import Home from "./component/Home";
 import Footer from "./component/Footer";
+import Contact from "./component/Contact";
 
 import "./styles/App.scss";
 import "./styles/header.scss";
 import "./styles/home.scss"
+import "./styles/footer.scss"
 function App() {
 
   return (
@@ -14,6 +16,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer/>
       </Router>
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      Footer
+    <footer>
+        <div>
+     <h1>TechyStar</h1>
+     <p>@all right reserved</p>
     </div>
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://github.com/Vikalp-Mishra03" target='blank'>Github</a>
+            <a href="https://www.instagram.com/vikalp_003/?hl=en" target='blank'>Instagram</a>
+            <a href="https://www.linkedin.com/in/vikalp-mishra" target='blank'>Linkedin</a>
+        </div>
+    </div>
+    </footer>
+
   )
 }
